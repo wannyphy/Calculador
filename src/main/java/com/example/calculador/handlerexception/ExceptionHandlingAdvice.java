8a9c9de7f6cfc4a466378a379e4ed12c7c77c436
Wanny <wannydev@gmail.com>
2023-05-24 11:43:28 +0200

    sube y corrección ExceptionHandlingAdvice

--- src/main/java/com/example/calculador/handlerexception/ExceptionHandlingAdvice.java
+++ src/main/java/com/example/calculador/handlerexception/ExceptionHandlingAdvice.java
@@ -1,5 +1,7 @@
 package com.example.calculador.handlerexception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import com.example.calculador.custom.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +11,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandlingAdvice {
 
+    private static final Logger logger = LogManager.getLogger(ExceptionHandlingAdvice.class);
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        ex.printStackTrace();
+    public ResponseEntity<String> handleException(Exception e) {
+        logger.error("Error en el servidor", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
     }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handleCustomException(CustomException ex) {
-        ex.printStackTrace();
+        logger.error(ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
